3561a19a8c7c4ca89bf6aaf3ea57bdf44f554917
abdulahiredwan <abdulahiredwan@gmail.com>
2021-06-23 06:04:59 +0300

    change az page and add contact telegram

--- resources/views/az.blade.php
+++ resources/views/az.blade.php
@@ -3,35 +3,24 @@
 @extends('layout')
 
 @section('content')
-
+    <h1>About Page </h1>
     <table class="table table-dark">
         <thead>
         <tr>
             <th scope="col">#</th>
             <th scope="col">First</th>
             <th scope="col">Last</th>
-            <th scope="col">Handle</th>
+            <th scope="col">contact</th>
         </tr>
         </thead>
         <tbody>
         <tr>
             <th scope="row">1</th>
-            <td>Mark</td>
-            <td>Otto</td>
-            <td>@mdo</td>
+            <td>Abdulahi</td>
+            <td>Redwan</td>
+            <td><a href="https://t.me/abd_red080">@abd_red080</a></td>
         </tr>
-        <tr>
-            <th scope="row">2</th>
-            <td>Jacob</td>
-            <td>Thornton</td>
-            <td>@fat</td>
-        </tr>
-        <tr>
-            <th scope="row">3</th>
-            <td>Larry</td>
-            <td>the Bird</td>
-            <td>@twitter</td>
-        </tr>
+       
         </tbody>
     </table>
 @endsection
